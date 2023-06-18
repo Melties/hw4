5cc6de2f94211346a7ef9bf18a7bc5eaffb5552c
Melties <131888470+Melties@users.noreply.github.com>
2023-06-18 23:21:46 +0300

    Update Task9.java

--- src/Task9.java
+++ src/Task9.java
@@ -1,3 +1,26 @@
+import java.io.*;
+
 public class Task9 {
+    public static void main(String[] args) {
+        Box box1 = new Box(4, 2, 3);
+        Box box2 = new Box(2, 8, 4);
+        Box box3 = new Box(4, 2, 3);
+        Box box4 = new Box(1, 4, 3);
+        Box box5 = new Box(2, 5, 2);
+        try {
+            FileOutputStream fos =
+                    new FileOutputStream("/Users/maria/Documents/Java/hw4/src/forBox.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(box1);
+            oos.writeObject(box2);
+            oos.writeObject(box3);
+            oos.writeObject(box4);
+            oos.writeObject(box5);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
-//    Создать класс коробка (Box(x,y,z)). Создать 5 объектов. Сериализовать их в файл.
+
