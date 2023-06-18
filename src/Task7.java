abd35f822e68dc22ba2ce0e423cc215f72820880
Melties <131888470+Melties@users.noreply.github.com>
2023-06-18 23:19:08 +0300

    Delete Task7.java

--- src/Task7.java
+++
@@ -1,9 +0,0 @@
-// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Task7 {
-    public static void main(String[] args) {
-
-    }
-}
-//    Прочитать файл (INPUT), в выходной файл (OUTPUT) записать все
-//        строки из первого, но в обратном порядке (первая строка должна быть последней).
