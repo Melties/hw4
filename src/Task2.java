1d8fcd3ee1e0146628df7641f73ff412f3dec733
Melties <131888470+Melties@users.noreply.github.com>
2023-06-18 23:20:12 +0300

    Update Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int sum = 0; int counter = 0;
-        String input = "";
-        while(!input.equals("stop")) {
-            input = in.nextLine();
-            sum += Integer.valueOf(input);
-            counter++;
+        int sum = 0;
+        int counter = 0;
+        while (true) {
+            String input = in.nextLine();
+            if (input.equals("stop")) {
+                break;
+            } else {
+                if(input.matches("[-+]?\\d+")) {
+                    counter++;
+                    sum += Integer.parseInt(input);
+                }
+            }
         }
         System.out.println(sum / counter);
         in.close();
     }
 }
+
