c29dfd5ea3027fb8a44fda00a41737d203da53cc
Melties <131888470+Melties@users.noreply.github.com>
2023-06-18 23:21:01 +0300

    Update Task4.java

--- src/Task4.java
+++ src/Task4.java
@@ -4,19 +4,32 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int res = 0;
-        int input = in.nextInt();
+        System.out.println("Enter first number");
+        String num1 = in.nextLine();
+        System.out.println("Enter second number");
+        String num2 = in.nextLine();
+        System.out.println("Enter sign of operation between them");
         String sign = in.nextLine();
-        if(sign.equals("+")) {
-            res = input + in.nextInt();
-        } else if(sign.equals("-")) {
-            res = input - in.nextInt();
-        } else if(sign.equals("*")) {
-            res = input * in.nextInt();
+        if (num1.matches("[-+]?\\d+") && num2.matches("[-+]?\\d+")) {
+            if(sign.equals("+")) {
+                res = Integer.parseInt(num1) + Integer.parseInt(num2);
+                System.out.println(num1 + " + " + num2 + " = " + res);
+            }
+            if(sign.equals("-")) {
+                res = Integer.parseInt(num1) - Integer.parseInt(num2);
+                System.out.println(num1 + " - " + num2 + " = " + res);
+            }
+            if(sign.equals("*")) {
+                res = Integer.parseInt(num1) * Integer.parseInt(num2);
+                System.out.println(num1 + " * " + num2 + " = " + res);
+            }
+            if(sign.equals("/")) {
+                res = Integer.parseInt(num1) / Integer.parseInt(num2);
+                System.out.println(num1 + " / " + num2 + " = " + res);
+            }
+        } else {
+            System.out.println("Invalid input");
         }
-        System.out.println(res);
         in.close();
     }
 }
-//    Необходимо реализовать программу "калькулятор".
-//        Поддерживаемые операции +-/* выбор операции и ввод данных
-//осуществляется пользователем с клавиатуры.
